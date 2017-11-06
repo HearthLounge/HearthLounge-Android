5f45dca0e7ecb58554270d700d7f62a4f818cca3
root <root@Turbo-komputer.localdomain>
2017-11-06 23:23:00 +0100

    Test04

--- app/src/main/java/pl/pjwstk/pgmd/hearthlounge/CardsJSON.java
+++ app/src/main/java/pl/pjwstk/pgmd/hearthlounge/CardsJSON.java
@@ -48,7 +48,7 @@ public class CardsJSON extends AppCompatActivity{
     private ListView listViewCards;
     private Button button;
     private TextView textCards;
-    private String obelga = "Zapomniałeś dodać, że ona pójdzie na policje :/ ";
+    private String obelga = "Ale policja nie przywróci jej rozumu :( ";
     RequestQueue requestQueue;
 
     @Override
